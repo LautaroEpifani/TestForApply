95711f0218ad644e0b4f979e39281dcae21513cc
Lautaro <epifanilautaro@gmail.com>
2023-02-20 15:44:55 +0100

    news

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import Posts from './components/Posts'
 import axios from 'axios'
 import {MdDeleteOutline} from 'react-icons/md'
 import NewPost from './components/NewPost';
@@ -10,35 +9,39 @@ interface IPost {
     title: string;
     userId: number;
 }
+// const initialPost = {
+//   title: "",
+//   body: "",
+//   userId: 1,
+//   id: 1
+  
+// };
 
 
 function App() {
   const [postsData, setPostsData] = useState([])
   const [active, setActive] = useState(false)
+  const [newPost, setNewPost] = useState<IPost>()
 
   const deletePost = (postId: number) => {
     const filteredPosts = postsData.filter((post: IPost) => post.id !== postId)
     setPostsData(filteredPosts)
   }
 
-  
-
   useEffect(() => {
     const getData = axios.get('https://jsonplaceholder.typicode.com/posts/')
-    .then((response) => setPostsData(response.data.splice(0,10)))
+    .then((response) => setPostsData(response.data.splice(90,100)))
     .catch(function (error) {
       console.log(error);
     });
   }, [])
-  
   return (
     <div className="">
     <h1 className="text-center mt-2 font-bold text-gray-600 underline">Posts</h1>
-     {/* <Posts postsData={postsData}/> */}
       <div className="lg:w-1/2 mx-auto grid grid-cols-1 gap-2 p-10">
         {postsData.map((post: IPost, index) => (
-          <div className="flex justify-between gap-2 ">
-            <div key={post.id} className="flex h-20 lg:h-auto w-full gap-4 font-semibold text-gray-500 justify-left items-center border border-gray-400 rounded py-2 px-8">
+          <div key={post.id} className="flex justify-between gap-2 ">
+            <div className="flex h-20 lg:h-auto w-full gap-4 font-semibold text-gray-500 justify-left items-center border border-gray-400 rounded py-2 px-8">
               <p>{post.id}</p><p>{post.title}</p>
             </div>
             <button className="w-10 border bg-blue-500 rounded font-semibold text-white uppercase flex justify-center items-center" onClick={() => deletePost(post.id)}><MdDeleteOutline/></button>
--- src/components/NewPost.tsx
+++ src/components/NewPost.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { ChangeEvent, FormEvent, useState, useRef } from 'react'
-import axios from 'axios'
+import React from "react";
+import { ChangeEvent, FormEvent, useState, useRef } from "react";
+import { MdDeleteOutline } from "react-icons/md";
+import axios from "axios";
 
 const initialPost = {
   title: "",
   body: "",
   userId: 1,
-  
+  id: null,
 };
 
 interface IPosts {
-      title: string;
-      body: string;
-      userId: number;
-      
+  title: string;
+  body: string;
+  userId: number;
+  id: undefined | number;
 }
 
 const NewPost = () => {
-
-  const [posts, setPosts] = useState<IPosts[]>([])
+  const [posts, setPosts] = useState<IPosts[]>([]);
   const [post, setPost] = useState(initialPost);
+
+  const deletePost = (postId: number | undefined) => {
+    const filteredPosts = posts.filter((post: IPosts) => post.id !== postId);
+    setPosts(filteredPosts);
+  };
 
   const handleChange = (
     e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -30,43 +35,52 @@ const NewPost = () => {
     axios
       .post("https://jsonplaceholder.typicode.com/posts", post)
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
+        setPosts([...posts, res.data]);
       });
-    setPosts([...posts, post])
-    setPost(initialPost)
-}
-console.log(posts)
+    setPost(initialPost);
+  };
+
   return (
     <div>
-        <form onSubmit={handleSubmit} className=" rounded-md">
-          <input
-            type="text"
-            placeholder="Title"
-            name="title"
-            onChange={handleChange}
-            value={post.title}
-            className="border border-gray-500 px-3 py-2 block w-full rounded-md mb-3"
-          />
-          <textarea
-            placeholder="Body"
-            name="body"
-            onChange={handleChange}
-            value={post.body}
-            className="border border-gray-500 px-3 py-2 block w-full rounded-md mb-3"
-            rows={3}
-          />
+      <form onSubmit={handleSubmit} className=" rounded-md">
+        <input
+          type="text"
+          placeholder="Title"
+          name="title"
+          onChange={handleChange}
+          value={post.title}
+          className="border border-gray-500 px-3 py-2 block w-full rounded-md mb-3"
+        />
+        <textarea
+          placeholder="Body"
+          name="body"
+          onChange={handleChange}
+          value={post.body}
+          className="border border-gray-500 px-3 py-2 block w-full rounded-md mb-3"
+          rows={3}
+        />
 
+        <button className="bg-blue-500 px-3 py-2 rounded-md text-white">
+          Save
+        </button>
+      </form>
+      {posts.map((post: IPosts, index) => (
+        <div key={post.id} className="flex justify-between gap-2 mt-4">
+          <div className="flex h-20 lg:h-auto w-full gap-4 font-semibold text-gray-500 justify-left items-center border border-gray-400 rounded py-2 px-8">
+            <p>{post.id}</p>
+            <p>{post.title}</p>
+          </div>
           <button
-            className="bg-blue-500 px-3 py-2 rounded-md text-white"
-          >Save</button>
-        </form>
-        {posts.map((item, index) => (
-              <div key={item.userId} className="flex w-full gap-4 font-semibold text-gray-500 justify-center border border-gray-400 rounded py-2 px-8 mt-4">
-                <p>{item.userId}</p><p>{item.title}</p>
-             </div>
-        ))}
-      </div>
-  )
-}
+            className="w-10 border bg-blue-500 rounded font-semibold text-white uppercase flex justify-center items-center"
+            onClick={() => deletePost(post.id)}
+          >
+            <MdDeleteOutline />
+          </button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default NewPost
+export default NewPost;
