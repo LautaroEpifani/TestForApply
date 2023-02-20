643be61a6083f55f3ca2880e270cad642bc3453d
Lautaro <epifanilautaro@gmail.com>
2023-02-20 15:45:44 +0100

    news

--- src/App.tsx
+++ src/App.tsx
@@ -1,60 +1,64 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
-import {MdDeleteOutline} from 'react-icons/md'
-import NewPost from './components/NewPost';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { MdDeleteOutline } from "react-icons/md";
+import NewPost from "./components/NewPost";
 
 interface IPost {
-    body: string;
-    id: number;
-    title: string;
-    userId: number;
+  body: string;
+  id: number;
+  title: string;
+  userId: number;
 }
-// const initialPost = {
-//   title: "",
-//   body: "",
-//   userId: 1,
-//   id: 1
-  
-// };
-
 
 function App() {
-  const [postsData, setPostsData] = useState([])
-  const [active, setActive] = useState(false)
-  const [newPost, setNewPost] = useState<IPost>()
+  const [postsData, setPostsData] = useState([]);
+  const [active, setActive] = useState(false);
 
   const deletePost = (postId: number) => {
-    const filteredPosts = postsData.filter((post: IPost) => post.id !== postId)
-    setPostsData(filteredPosts)
-  }
+    const filteredPosts = postsData.filter((post: IPost) => post.id !== postId);
+    setPostsData(filteredPosts);
+  };
 
   useEffect(() => {
-    const getData = axios.get('https://jsonplaceholder.typicode.com/posts/')
-    .then((response) => setPostsData(response.data.splice(90,100)))
-    .catch(function (error) {
-      console.log(error);
-    });
-  }, [])
+    const getData = axios
+      .get("https://jsonplaceholder.typicode.com/posts/")
+      .then((response) => setPostsData(response.data.splice(90, 100)))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
   return (
     <div className="">
-    <h1 className="text-center mt-2 font-bold text-gray-600 underline">Posts</h1>
+      <h1 className="text-center mt-2 font-bold text-gray-600 underline">
+        Posts
+      </h1>
       <div className="lg:w-1/2 mx-auto grid grid-cols-1 gap-2 p-10">
         {postsData.map((post: IPost, index) => (
           <div key={post.id} className="flex justify-between gap-2 ">
             <div className="flex h-20 lg:h-auto w-full gap-4 font-semibold text-gray-500 justify-left items-center border border-gray-400 rounded py-2 px-8">
-              <p>{post.id}</p><p>{post.title}</p>
+              <p>{post.id}</p>
+              <p>{post.title}</p>
             </div>
-            <button className="w-10 border bg-blue-500 rounded font-semibold text-white uppercase flex justify-center items-center" onClick={() => deletePost(post.id)}><MdDeleteOutline/></button>
+            <button
+              className="w-10 border bg-blue-500 rounded font-semibold text-white uppercase flex justify-center items-center"
+              onClick={() => deletePost(post.id)}
+            >
+              <MdDeleteOutline />
+            </button>
           </div>
         ))}
-        <button onClick={() => setActive(!active)} className="w-44 border bg-blue-500 rounded font-semibold text-white uppercase flex justify-center items-center py-2 mt-2">New Post</button>
+        <button
+          onClick={() => setActive(!active)}
+          className="w-44 border bg-blue-500 rounded font-semibold text-white uppercase flex justify-center items-center py-2 mt-2"
+        >
+          New Post
+        </button>
         <div className={active ? `` : `hidden`}>
-          <NewPost/>
+          <NewPost />
         </div>
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
